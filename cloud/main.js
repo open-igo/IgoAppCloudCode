509e721e787a5505336d8461b62c93f815a18d57
shigeshibu44 <shigeshibu44@gmail.com>
2015-05-20 08:58:57 +0900

    add search function

--- cloud/main.js
+++ cloud/main.js
@@ -6,10 +6,10 @@ Parse.Cloud.define("hello", function(request, response) {
   query.equalTo("objectId", request.params.id);
   query.find({
     success: function(results) {
-      response.success(_.VERSION);
+      response.success(new Date());
     },
     error: function() {
-      response.error("cant_find_tournament_data");
+      response.error(new Date());
     }
   });
 });
@@ -20,23 +20,41 @@ Parse.Cloud.define("before10TournamentData", function(request, response) {
   query.find({
     success: function(results) {
       var targetDate = results[0].get("tournament_date");
-      var createdDate = results[0].get("createdAt");
       var query2 = new Parse.Query("tournament_mst");
       query2.lessThanOrEqualTo("tournament_date", targetDate);
-      query2.addAscending("tournament_date,createdAt");
+      query2.addAscending("tournament_date,objectId");
       query2.find({
         success:function(results){
 
           // 同じ開催日付のデータを考慮
           var before10 = _.filter(results,function(tournament){
-            return !(tournament.get("tournament_date") === targetDate && tournament.get("objectId") >= request.params.id);
+            return !(tournament.get("tournament_date") == targetDate && tournament.id >= request.params.id);
           }).slice(-10);
-          response.success(before10);
+          response.success(before10.length);
         }
       });
 
     },
     error: function() {
+      response.error("cant_find_tournament_data");
+    }
+  });
+});
+
+Parse.Cloud.define("searchTournamentData", function(request,response){
+
+  var params = request.params;
+
+  var query = new Parse.Query("tournament_mst");
+  if("pref" in params && params.pref.length > 0) query.contains("pref_key",params.pref);
+  if("guest_status" in params && params.guest_status.length > 0) query.contains("guest_status_key",params.guest_status);
+  if("year_month" in params && params.year_month.length > 0) query.greaterThanOrEqualTo("tournament_date",params.year_month);
+  query.addAscending("tournament_date,objectId");
+  query.find({
+    success: function(results){
+      response.success(results.slice(-20));
+    },
+    error: function(){
       response.error("cant_find_tournament_data");
     }
   });
