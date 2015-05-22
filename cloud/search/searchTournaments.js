e1e4dd2a68a033ca3d02863f9bf2055ffa495477
shigeshibu44 <shigeshibu44@gmail.com>
2015-05-22 20:31:35 +0900

    recover all pref query

--- cloud/search/searchTournaments.js
+++ cloud/search/searchTournaments.js
@@ -15,7 +15,12 @@ Parse.Cloud.define("getBefore20Tournaments", function(request, response) {
 
       var targetDate = results[0].get("tournament_date_key");
 
-      var query2 = new Parse.Query("tournament_mst");
+      var prefQuery = new Parse.Query("tournament_mst");
+      if("pref" in params && params.pref.length > 0) prefQuery.contains("pref_key",params.pref);
+      var allPrefQuery = new Parse.Query("tournament_mst");
+      if("pref" in params && params.pref.length > 0) allPrefQuery.contains("pref_key","全国");
+
+      var query2 = Parse.Query.or(prefQuery,allPrefQuery);
 
       query2.lessThanOrEqualTo("tournament_date_key", targetDate);
       if("pref" in params && params.pref.length > 0) query2.contains("pref_key",params.pref);
@@ -55,10 +60,14 @@ Parse.Cloud.define("getAfter20Tournaments", function(request, response) {
 
       var targetDate = results[0].get("tournament_date_key");
 
-      var query2 = new Parse.Query("tournament_mst");
+      var prefQuery = new Parse.Query("tournament_mst");
+      if("pref" in params && params.pref.length > 0) prefQuery.contains("pref_key",params.pref);
+      var allPrefQuery = new Parse.Query("tournament_mst");
+      if("pref" in params && params.pref.length > 0) allPrefQuery.contains("pref_key","全国");
+
+      var query2 = Parse.Query.or(prefQuery,allPrefQuery);
 
       query2.greaterThanOrEqualTo("tournament_date_key", targetDate);
-      if("pref" in params && params.pref.length > 0) query2.contains("pref_key",params.pref);
       if("guest_status" in params && params.guest_status.length > 0) query2.contains("guest_status_key",params.guest_status);
 
       query2.addAscending("tournament_date_key,objectId");
@@ -84,8 +93,13 @@ Parse.Cloud.define("searchTournaments", function(request,response){
 
   var params = request.params;
 
-  var query = new Parse.Query("tournament_mst");
-  if("pref" in params && params.pref.length > 0) query.contains("pref_key",params.pref);
+  var prefQuery = new Parse.Query("tournament_mst");
+  if("pref" in params && params.pref.length > 0) prefQuery.contains("pref_key",params.pref);
+  var allPrefQuery = new Parse.Query("tournament_mst");
+  if("pref" in params && params.pref.length > 0) allPrefQuery.contains("pref_key","全国");
+
+  var query = Parse.Query.or(prefQuery,allPrefQuery);
+
   if("guest_status" in params && params.guest_status.length > 0) query.contains("guest_status_key",params.guest_status);
   if("year_month" in params && params.year_month.length > 0) query.greaterThanOrEqualTo("tournament_date_key",params.year_month);
   query.addAscending("tournament_date,objectId");
