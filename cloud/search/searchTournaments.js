2a0dafca3fc18c6130e21c67e4d9726d13b3fb92
shigeshibu44 <shigeshibu44@gmail.com>
2015-05-22 19:40:01 +0900

    recover after and before search

--- cloud/search/searchTournaments.js
+++ cloud/search/searchTournaments.js
@@ -3,26 +3,31 @@ Parse.Cloud.define("getBefore20Tournaments", function(request, response) {
   var query = new Parse.Query("tournament_mst");
   var params = request.params;
 
+  if(params.id == null || params.id.length < 1) {
+    response.success([]);
+    return;
+  }
+
   query.equalTo("objectId", params.id);
 
   query.find({
     success: function(results) {
 
-      var targetDate = results[0].get("tournament_date");
+      var targetDate = results[0].get("tournament_date_key");
 
       var query2 = new Parse.Query("tournament_mst");
 
-      query2.lessThanOrEqualTo("tournament_date", targetDate);
+      query2.lessThanOrEqualTo("tournament_date_key", targetDate);
       if("pref" in params && params.pref.length > 0) query2.contains("pref_key",params.pref);
       if("guest_status" in params && params.guest_status.length > 0) query2.contains("guest_status_key",params.guest_status);
 
-      query2.addAscending("tournament_date,objectId");
+      query2.addAscending("tournament_date_key,objectId");
       query2.find({
         success:function(results){
 
           // 同じ開催日付のデータを考慮
           var before20 = _.filter(results,function(tournament){
-            return !(tournament.get("tournament_date") == targetDate && tournament.id >= request.params.id);
+            return !(tournament.get("tournament_date_key") == targetDate && tournament.id >= request.params.id);
           }).slice(-20);
           response.success(before20);
         }
@@ -39,26 +44,30 @@ Parse.Cloud.define("getAfter20Tournaments", function(request, response) {
   var query = new Parse.Query("tournament_mst");
   var params = request.params;
 
+  if(params.id == null || params.id.length < 1) {
+    response.success([]);
+    return;
+  }
   query.equalTo("objectId", params.id);
 
   query.find({
     success: function(results) {
 
-      var targetDate = results[0].get("tournament_date");
+      var targetDate = results[0].get("tournament_date_key");
 
       var query2 = new Parse.Query("tournament_mst");
 
-      query2.greaterThanOrEqualTo("tournament_date", targetDate);
+      query2.greaterThanOrEqualTo("tournament_date_key", targetDate);
       if("pref" in params && params.pref.length > 0) query2.contains("pref_key",params.pref);
       if("guest_status" in params && params.guest_status.length > 0) query2.contains("guest_status_key",params.guest_status);
 
-      query2.addAscending("tournament_date,objectId");
+      query2.addAscending("tournament_date_key,objectId");
       query2.find({
         success:function(results){
 
           // 同じ開催日付のデータを考慮
           var after20 = _.filter(results,function(tournament){
-            return !(tournament.get("tournament_date") == targetDate && tournament.id <= request.params.id);
+            return !(tournament.get("tournament_date_key") == targetDate && tournament.id <= request.params.id);
           }).slice(0,20);
           response.success(after20);
         }
@@ -78,11 +87,11 @@ Parse.Cloud.define("searchTournaments", function(request,response){
   var query = new Parse.Query("tournament_mst");
   if("pref" in params && params.pref.length > 0) query.contains("pref_key",params.pref);
   if("guest_status" in params && params.guest_status.length > 0) query.contains("guest_status_key",params.guest_status);
-  if("year_month" in params && params.year_month.length > 0) query.greaterThanOrEqualTo("tournament_date",params.year_month);
+  if("year_month" in params && params.year_month.length > 0) query.greaterThanOrEqualTo("tournament_date_key",params.year_month);
   query.addAscending("tournament_date,objectId");
   query.find({
     success: function(results){
-      response.success(results.slice(-20));
+      response.success(results.slice(0,20));
     },
     error: function(){
       response.error("cant_find_tournament_data");
